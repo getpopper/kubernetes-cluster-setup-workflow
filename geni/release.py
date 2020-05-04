f89d0f7024a6be8b92dfae6f91743bb72f7cfda5
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-04 19:27:47 +0530

    allow specifying slicename and cluster (#9)

--- geni/release.py
+++ geni/release.py
@@ -3,15 +3,27 @@ import os
 from geni.aggregate import cloudlab
 from geni import util
 
+slice_name = str(os.environ['NODE_SLICE_NAME'])
 
 ctx = util.loadContext("/geni-context.json",
                        key_passphrase=os.environ['GENI_KEY_PASSPHRASE'])
 
 print("Available slices: {}".format(ctx.cf.listSlices(ctx).keys()))
 
-if util.sliceExists(ctx, 'kube-cluster'):
+# identify the cluster
+cluster = str(os.environ['NODE_CLUSTER'])
+if cluster == 'utah':
+    cluster = cloudlab.Utah
+
+elif cluster == 'wisconsin':
+    cluster = cloudlab.Wisconsin
+
+elif cluster == 'clemson':
+    cluster = cloudlab.Clemson
+
+if util.sliceExists(ctx, slice_name):
     print('Slice exists.')
     print('Removing all existing slivers (errors are ignored)')
-    util.deleteSliverExists(cloudlab.Utah, ctx, 'kube-cluster')
+    util.deleteSliverExists(cluster, ctx, slice_name)
 else:
     print("Slice does not exist.")
