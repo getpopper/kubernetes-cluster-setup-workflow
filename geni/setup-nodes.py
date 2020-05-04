d56b1ec740f2aab05afc0851247bc6207d728626
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-04 17:31:45 +0530

    Allow specifying count and hardware of nodes

--- geni/setup-nodes.py
+++ geni/setup-nodes.py
@@ -14,18 +14,17 @@ from geni.aggregate import cloudlab
 
 request = portal.context.makeRequestRSpec()
 
-# Create three raw "PC" nodes
-node1 = request.RawPC("node1")
-node2 = request.RawPC("node2")
-node3 = request.RawPC("node3")
+node_list = list()
+node_cnt = int(os.environ['NODES'])
 
-# Set each of the two to specifically request "m400" nodes, which in CloudLab, are ARM
-node1.hardware_type = "m400"
-node2.hardware_type = "m400"
-node3.hardware_type = "m400"
+# Create the raw "PC" nodes
+for _idx in range(0, node_cnt):
+    node = request.RawPC('node_{}'.format(_idx))
+    node.hardware_type = str(os.environ['NODE_HARDWARE'])
+    node_list.append(node)
 
 # Create a link between them
-link1 = request.Link(members = [node1, node2, node3])
+link1 = request.Link(members=node_list)
 
 # load context
 ctx = util.loadContext(path="/geni-context.json",
@@ -56,7 +55,7 @@ dict_response = xmltodict.parse(manifest.text)
 
 hosts_dict['masters']['hosts'] = {
     'master': {
-        'ansible_host': dict_response['rspec']['node'][0]['host']['@ipv4'],
+        'ansible_host': str(dict_response['rspec']['node'][0]['host']['@ipv4']),
         'ansible_user': os.environ['GENI_USERNAME']
     }
 }
@@ -64,7 +63,7 @@ hosts_dict['masters']['hosts'] = {
 worker_hosts = {}
 for _idx in range(1, len(dict_response['rspec']['node'])):
     worker_hosts['worker{}'.format(_idx)] = {
-        'ansible_host': dict_response['rspec']['node'][_idx]['host']['@ipv4'],
+        'ansible_host': str(dict_response['rspec']['node'][_idx]['host']['@ipv4']),
         'ansible_user': os.environ['GENI_USERNAME']
     }
 hosts_dict['workers']['hosts'] = worker_hosts
