7f88232f29c1284ae7b06b8b0024e6a8dee444c7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-04 19:09:20 +0530

    wip

--- geni/setup-nodes.py
+++ geni/setup-nodes.py
@@ -17,6 +17,8 @@ request = portal.context.makeRequestRSpec()
 node_list = list()
 node_cnt = int(os.environ['NODES'])
 
+slice_name = str(os.environ['NODE_SLICE_NAME'])
+
 # Create the raw "PC" nodes
 for _idx in range(0, node_cnt):
     node = request.RawPC('node_{}'.format(_idx))
@@ -31,10 +33,21 @@ ctx = util.loadContext(path="/geni-context.json",
                        key_passphrase=os.environ['GENI_KEY_PASSPHRASE'])
 
 # create slice
-util.createSlice(ctx, 'kube-cluster')
+util.createSlice(ctx, slice_name)
 
-# create sliver on utah
-manifest = util.createSliver(ctx, cloudlab.Utah, 'kube-cluster', request)
+# identify the cluster
+cluster = str(os.environ['NODE_CLUSTER'])
+if cluster == 'utah':
+    cluster = cloudlab.Utah
+
+elif cluster == 'wisconsin':
+    cluster = cloudlab.Wisconsin
+
+elif cluster == 'clemson':
+    cluster = cloudlab.Clemson
+
+# create sliver
+manifest = util.createSliver(ctx, cluster, slice_name, request)
 
 # generate ansible/hosts.yml file
 hosts_dict = {
@@ -56,7 +69,7 @@ dict_response = xmltodict.parse(manifest.text)
 hosts_dict['masters']['hosts'] = {
     'master': {
         'ansible_host': str(dict_response['rspec']['node'][0]['host']['@ipv4']),
-        'ansible_user': os.environ['GENI_USERNAME']
+        'ansible_user': str(os.environ['GENI_USERNAME'])
     }
 }
 
@@ -64,7 +77,7 @@ worker_hosts = {}
 for _idx in range(1, len(dict_response['rspec']['node'])):
     worker_hosts['worker{}'.format(_idx)] = {
         'ansible_host': str(dict_response['rspec']['node'][_idx]['host']['@ipv4']),
-        'ansible_user': os.environ['GENI_USERNAME']
+        'ansible_user': str(os.environ['GENI_USERNAME'])
     }
 hosts_dict['workers']['hosts'] = worker_hosts
 
